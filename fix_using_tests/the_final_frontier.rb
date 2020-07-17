d5ff5a9830df071bfe02f4faed1326cd8ba932f7
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-17 21:58:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,10 +7,20 @@ end
 
 
 def state_log(star_date)
-  "/Captain's Log, star date/ #{star_date}."
+  "Captain's Log, star date #{star_date}."
+end
+
+def crew 
+  ["Gregory", "Mike", "Richard", "Will", "Brandi", "Wanda"]
+end
+
+def greet_crew (crew) 
+  crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
   date= generate_star_date
   puts state_log(date)
 end
+
+return engage
